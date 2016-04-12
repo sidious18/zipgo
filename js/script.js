f153163cad68a08ba9c74146663fbea4cd72f595
sidious18 <max_leb@mail.ru>
2016-04-12 18:08:10 +0300

    ready 1html

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 jQuery(document).ready(function($) {
-
+  $( "#company-city" ).selectmenu();
+  //background for history-page
   if ($(".my-rides-box .ride-box").length > 1){
     var position = "background-position: " + "7px " + $(".my-rides-box .ride-box:first-child").outerHeight()/2+"px; "
     var size = "background-size: " + "2px " + ($(".my-rides-box").outerHeight() - $(".my-rides-box .ride-box:last-child").outerHeight()/2 - $(".my-rides-box .ride-box:first-child").outerHeight()/2)+"px";
@@ -8,4 +9,5 @@ jQuery(document).ready(function($) {
   else{
     $(".my-rides-box").css("background", "none");
   }
+
 });
