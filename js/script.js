3b375c11a1e016b8197a3417e370257d5c60ec15
sidious18 <max_leb@mail.ru>
2016-04-12 13:15:11 +0300

    ready history page

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
+jQuery(document).ready(function($) {
 
+  if ($(".my-rides-box .ride-box").length > 1){
+    var position = "background-position: " + "7px " + $(".my-rides-box .ride-box:first-child").outerHeight()/2+"px; "
+    var size = "background-size: " + "2px " + ($(".my-rides-box").outerHeight() - $(".my-rides-box .ride-box:last-child").outerHeight()/2 - $(".my-rides-box .ride-box:first-child").outerHeight()/2)+"px";
+    $(".my-rides-box").attr("style", position + size);
+  }
+  else{
+    $(".my-rides-box").css("background", "none");
+  }
+});
